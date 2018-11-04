9cbcad1e56de4673d3ac79a46d3ee45fddeb42fc
shikanon520 <enming_shi@dataspark.cn>
2018-11-05 00:32:09 +0800

    删除e2e_test中的冗余代码

--- e2e_test.go
+++ e2e_test.go
@@ -3,7 +3,6 @@ package socks5proxy
 import (
 	"testing"
 	"time"
-	"math/rand"
 	"github.com/stretchr/testify/assert"
 	"sync"
 	"log"
@@ -11,51 +10,13 @@ import (
 )
 
 
-func connect(packSize int){
-	data := make([]byte, packSize)
-	// 生成随机字符写入data, 返回len(data),error
-	_, err := rand.Read(data)
-	if err != nil{
-		log.Fatal(err)
-	}
-	go Server("127.0.0.1:18189", "random", "abcedfg")
-	go Client("127.0.0.1:18190", "127.0.0.1:18189", "random", "abcedfg")
-
-	time.Sleep(1 * time.Second)
-
-	// 连接
-	conn, err := net.Dial("tcp", "127.0.0.1:18190")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	readResult := make([]byte, 2)
-	wg := new(sync.WaitGroup)
-    wg.Add(2)
-
-	// 写
-	go func() {
-		defer wg.Done()
-		conn.Write([]byte{0x05, 0x01, 0x00})
-	}()
-
-	// 读
-	go func(){
-		defer wg.Done()
-		conn.Read(readResult)
-	}()
-
-	wg.Wait()
-	log.Println(readResult)
-	// assert.Equal(t, readResult, []byte{0x05, 0x00})
-}
-
 func TestConncet(t *testing.T){
 	go Server("127.0.0.1:18189", "random", "abcedfg")
 	go Client("127.0.0.1:18190", "127.0.0.1:18189", "random", "abcedfg")
 
-	// 连接
+	time.Sleep(1 * time.Second)
+	
+    // 连接
 	conn, err := net.Dial("tcp", "127.0.0.1:18190")
 	if err != nil {
 		log.Fatalln(err)
