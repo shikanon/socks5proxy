e18a29476f049a9ca41f6a859513e689ad86468d
South China University <shikanon@users.noreply.github.com>
2026-06-07 19:29:26 +0800

    security: require explicit password flags (#32)
    Closes #17

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,9 +8,12 @@ import (
 
 func main() {
 	listenAddr := flag.String("local", ":18888", "Input server listen address(Default 8888):")
-	passwd := flag.String("passwd", "123456", "Input server proxy password:")
+	passwd := flag.String("passwd", "", "Input server proxy password:")
 	encrytype := flag.String("type", "random", "Input encryption type:")
 	flag.Parse()
+	if *passwd == "" {
+		log.Fatal("请通过 -passwd 设置一个强密码（不能为空）")
+	}
 	log.Println("服务器正在启动...")
 	socks5proxy.Server(*listenAddr, *encrytype, *passwd)
 }
